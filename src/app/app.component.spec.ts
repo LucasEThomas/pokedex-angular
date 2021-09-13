165d61d061a888024136da7b4d41e3818f2e0708
Lucas Thomas <nospam4mester@gmail.com>
2021-09-13 11:01:33 -0500

    I can spell pokemon

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'pokidex-angular'`, () => {
+  it(`should have as title 'pokedex-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('pokidex-angular');
+    expect(app.title).toEqual('pokedex-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('pokidex-angular app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('pokedex-angular app is running!');
   });
 });
